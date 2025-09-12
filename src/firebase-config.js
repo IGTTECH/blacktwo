e1d02e7d6a4b27ceb5b664b5dad76541d36a2900
unknown <206084850+igtseth@users.noreply.github.com>
2025-09-12 13:45:37 +0300

    Update firebase-config.js
    Signed-off-by: unknown <206084850+igtseth@users.noreply.github.com>

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -13,10 +13,10 @@ export const firebaseConfig = {
 };
 */
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_PROJECT.firebaseapp.com",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "REPLACE_WITH_SENDER_ID",
-  appId: "REPLACE_WITH_APP_ID"
+  apiKey: "AIzaSyBGBL3C-zTw6vwvIhktUE6mFfanLa-emJA",
+  authDomain: "igt-project-12443.firebaseapp.com",
+  projectId: "igt-project-12443",
+  storageBucket: "igt-project-12443.firebasestorage.app",
+  messagingSenderId: "458543900333",
+  appId: "1:458543900333:web:5781a612406ef2dfe3c824"
 };
